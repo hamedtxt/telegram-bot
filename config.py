a699e2897e62587fa012a027ba6414e6bc2fd9c5
hamedtxt <hamed021.1368@gmail.com>
2025-03-25 13:57:34 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
-TELEGRAM_TOKEN = "7586893119:AAFuy84hPwL-0e0MXqSKvn9scFYngeSeVfY"
-PUBLIC_CHANNEL_ID = "@pronoisesmuisic"
-PRIVATE_CHANNEL_ID = "-1002660905080"  # کانال خصوصی
+TELEGRAM_TOKEN = ""
+PUBLIC_CHANNEL_ID = ""
+PRIVATE_CHANNEL_ID = ""  # کانال خصوصی
 MERCHANT_ID = "12345678-1234-1234-1234-123456789abc"  # 36 کاراکتر دلخواه
 SANDBOX_PAYMENT_URL = "https://sandbox.zarinpal.com/pg/v4/payment/request.json"
 SANDBOX_VERIFY_URL = "https://sandbox.zarinpal.com/pg/v4/payment/verify.json"
